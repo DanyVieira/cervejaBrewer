63ca96f67e8f7a8f80c9553d27f9e797e7f0b217
Daniele <danielevf@gmail.com>
2018-04-27 10:33:34 -0300

    Commit com hierarquia correta de paginas html e adicinando novas dependencias do Thymeleaf Spring BOOT

--- src/main/java/com/algaworks/controller/CervejasController.java
+++ src/main/java/com/algaworks/controller/CervejasController.java
@@ -9,6 +9,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.model.Cerveja;
@@ -19,13 +20,13 @@ public class CervejasController {
 	
 	@RequestMapping("/cervejas/novo")
 	 @GetMapping
-	 public String novo(Cerveja cerveja){
-		
+	 public ModelAndView novo(Cerveja cerveja){ 
+		ModelAndView mv = new ModelAndView("/cerveja/CadastroCerveja");
 		// model.addAttribute("cerveja",new Cerveja()); //crio a variavel cerveja para chama-la la na view
-		  return "layout/LayoutPadrao";
+		  return mv;
 	 }
 
-	@RequestMapping (value="cervejas/novo", method= RequestMethod.POST)
+	/*@RequestMapping (value="cervejas/novo", method= RequestMethod.POST)
 	public String cadastrar (@Valid Cerveja cerveja,   //valid adiciona a validação ao campo
 			BindingResult result,  //resultado do binding
 			Model model,  //para lançar a mensagem de erro 
@@ -33,13 +34,13 @@ public class CervejasController {
 			
 		if(result.hasErrors()){ //se ocorreu um erro no preenchimemto da variavel
 			//	model.addAttribute(cerveja);//pois nesse caso envio o campo que foi preenchido corretamante de volta para a view p não ter que digitar novamente
-				return novo(cerveja);
+				return mv;
 		}
 		attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso"); //agora consigo colocar uma mensagem em uma página redirect
 		System.out.println("cadastrar"+cerveja.getSku());//neste caso o spring ja preencheu o objeto com o valor passado automaticamente
 		return "redirect:/cervejas/novo"; //redirect : se deu tudo certo quero que ele va a tela de novo cadastro ! Ou seja faz um novo get para a pagina cadastro
 		
 	}
-	
+	*/
 	
 }
