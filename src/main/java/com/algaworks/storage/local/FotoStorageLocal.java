3161e72813f39e2c496230959d13dc4d4e388329
Daniele <danielevf@gmail.com>
2018-07-27 10:45:46 -0300

    Identificação de campos obrigatorios

--- src/main/java/com/algaworks/storage/local/FotoStorageLocal.java
+++ src/main/java/com/algaworks/storage/local/FotoStorageLocal.java
@@ -10,20 +10,22 @@ import java.nio.file.Path;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.algaworks.storage.FotoStorage;
 
+
 @Component
 public class FotoStorageLocal implements FotoStorage{
 	
-	private static final Logger logger = LoggerFactory.getLogger(FotoStorageLocal.class);
+	private static final Logger logger = LoggerFactory.getLogger(FotoStorageLocal.class); //tem que ser o logger do slf4j , usada pra fazer logger
 	private Path local;   //onde sera salvo a foto
-	private Path localTemporario;  //onde salva a foto enquanto faz o upload
+	private Path localTemporario;  //onde salva a foto enquanto faz o upload, ao clicar em salvar sai do temporario e vai pro local
 	
 
-	public FotoStorageLocal (){
+	public FotoStorageLocal (){  //cpnstrutor
 		this.local = FileSystems.getDefault().getPath(System.getenv("HOME"), ".brewerfotos"); // no home do usuário cria um diretorio chamado brewerfotos
 		criarPastas(); //chama o metodo criar pastas
 	}
@@ -31,7 +33,7 @@ public class FotoStorageLocal implements FotoStorage{
 	private void criarPastas() {
 		try {
 			Files.createDirectories(this.local); //cria diretorio local
-			this.localTemporario = FileSystems.getDefault().getPath(this.local.toString(), ".temp"); //dentro da pasta criada para local crio uma nova pasta chamada temp
+			this.localTemporario = FileSystems.getDefault().getPath(this.local.toString(), "temp"); //dentro da pasta criada para local crio uma nova pasta chamada temp
 			Files.createDirectories(this.localTemporario);  //cria diretorio temporario
 			
 			/// mensagens de sucesso
@@ -50,7 +52,7 @@ public class FotoStorageLocal implements FotoStorage{
 
 	@Override
 	public void salvarTemporariamente(MultipartFile[] files) {
-		System.out.println("salvando temporariamente");
+		System.out.println("salvando temporariamente>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 		
 	}
 	
